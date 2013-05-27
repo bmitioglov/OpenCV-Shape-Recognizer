89c176dd3276451e4a9d89f91655b11475599430
Mitioglov Boris <ibmttz13.5@gmail.com>
2013-05-27 22:40:42 +0400

    Работа над прямоугольником

--- DiplomApplication2/DiplomApplication2.cpp
+++ DiplomApplication2/DiplomApplication2.cpp
@@ -7,7 +7,7 @@
 
 // сравнение объектов по моментам их контуров 
 //templ - образец, original - набор фигур
-void testMatch(IplImage* original, IplImage* templ) 
+void testMatch(IplImage* original, IplImage* templCircle, IplImage* templRectangle) 
 {
         assert(original!=0);
         assert(templ!=0);
@@ -19,24 +19,27 @@ void testMatch(IplImage* original, IplImage* templ)
 
         src=cvCloneImage(original);
 		
-
+		
         IplImage* binI = cvCreateImage( cvGetSize(original), 8, 1);
-        IplImage* binT = cvCreateImage( cvGetSize(templ), 8, 1);
-				
+        IplImage* binCircle = cvCreateImage( cvGetSize(templCircle), 8, 1);
+		IplImage* binRectangle = cvCreateImage( cvGetSize(templRectangle), 8, 1);		
 
         // заведём цветные картинки
         IplImage* rgb = cvCreateImage(cvGetSize(original), 8, 3);
         cvConvertImage(src, rgb, CV_GRAY2BGR);
-        IplImage* rgbT = cvCreateImage(cvGetSize(templ), 8, 3);
-        cvConvertImage(templ, rgbT, CV_GRAY2BGR);
-
+        IplImage* rgbCircle = cvCreateImage(cvGetSize(templCircle), 8, 3);
+        cvConvertImage(templCircle, rgbCircle, CV_GRAY2BGR);
+		IplImage* rgbRectangle = cvCreateImage(cvGetSize(templRectangle), 8, 3);
+        cvConvertImage(templRectangle, rgbRectangle, CV_GRAY2BGR);
 
         // получаем границы изображения и шаблона
         cvCanny(src, binI, 50, 200);
-        cvCanny(templ, binT, 50, 200);
+        cvCanny(templCircle, binCircle, 50, 200);
+		cvCanny(templRectangle, binRectangle, 20, 200);
 
+	
 		IplImage* binI2 = cvCreateImage( cvGetSize(original), 8, 1);
-        IplImage* binT2 = cvCreateImage( cvGetSize(templ), 8, 1);
+        //IplImage* binCircle2 = cvCreateImage( cvGetSize(templCircle), 8, 1);
 
 		cvDilate(binI, binI2); 
 		cvErode(binI2, binI);
@@ -44,15 +47,18 @@ void testMatch(IplImage* original, IplImage* templ)
         cvNamedWindow( "cannyI", 1 );
         cvShowImage( "cannyI", binI);
 
-        cvNamedWindow( "cannyT", 1 );
-		cvShowImage( "cannyT", binT);
+        cvNamedWindow( "cannyCircle", 1 );
+		cvShowImage( "cannyCircle", binCircle);
+
+		cvNamedWindow("cannyRectangle", 1);
+		cvShowImage("cannyRectangle", binRectangle);
 
         // для хранения контуров
         CvMemStorage* storage = cvCreateMemStorage(0);
-        CvSeq* contoursI=0, *contoursT=0;
+        CvSeq* contoursI=0, *contoursCircle=0, *contoursRectangle=0;
 
-        // находим контуры изображения
-        int contoursCont = cvFindContours( binI, storage, &contoursI, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+        // находим контуры исходного изображения
+        int contoursICount = cvFindContours( binI, storage, &contoursI, sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
 
         // для отметки контуров
         CvFont font;
@@ -60,25 +66,45 @@ void testMatch(IplImage* original, IplImage* templ)
         char buf[1024];
         int counter=0;
         cvConvertImage(src, rgb, CV_GRAY2BGR);
-        // находим контуры шаблона
-        cvFindContours( binT, storage, &contoursT, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE , cvPoint(0,0));
-        CvSeq* seqT=0;
-        double perimT = 0;
+       
 
-		        if(contoursT!=0){
-                // ������� ����� ������� ������ 
-                for(CvSeq* seq0 = contoursT;seq0!=0;seq0 = seq0->h_next){
+		// находим самый длинный контур Круга
+        cvFindContours( binCircle, storage, &contoursCircle, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE , cvPoint(0,0));
+        CvSeq* seqCircle=0;
+        double perimCircle = 0;
+
+		        if(contoursCircle!=0){
+                for(CvSeq* seq0 = contoursCircle;seq0!=0;seq0 = seq0->h_next){
                         double perim = cvContourPerimeter(seq0);
-                        if(perim>perimT){
-                                perimT = perim;
-                                seqT = seq0;
+                        if(perim>perimCircle){
+                                perimCircle = perim;
+                                seqCircle = seq0;
                         }
-                        // ������
-                        cvDrawContours(rgbT, seqT, CV_RGB(255,216,0), CV_RGB(0,0,250), 0, 1, 8); // ������ ������
+                        cvDrawContours(rgbCircle, seqCircle, CV_RGB(255,216,0), CV_RGB(0,0,250), 0, 1, 8); 
                 }
         }
 
+		// находим самый длинный контур Прямоугольника
+		cvFindContours( binRectangle, storage, &contoursRectangle, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE , cvPoint(0,0));
+        CvSeq* seqRectangle=0;
+        double perimRectangle = 0;
 
+		        if(contoursRectangle!=0){
+					for(CvSeq* seq0 = contoursRectangle;seq0!=0;seq0 = seq0->h_next)
+					{
+                        double perim = cvContourPerimeter(seq0);
+                        if(perim>perimRectangle){
+                                perimRectangle = perim;
+                                seqRectangle = seq0;
+                        }
+                        cvDrawContours(rgbRectangle, seqRectangle, CV_RGB(255,216,0), CV_RGB(0,0,250), 0, 1, 8); 
+					}
+        }
+
+		
+				
+				
+		//1) Ищем для Круга--------------------------------------------------
         CvSeq* seqM=0;
         double matchMax=1000;
         // обходим контуры изображения 
@@ -87,7 +113,7 @@ void testMatch(IplImage* original, IplImage* templ)
         if(contoursI!=0){
                 // поиск лучшего совпадения контуров по их моментам 
                 for(CvSeq* seq0 = contoursI;seq0!=0;seq0 = seq0->h_next){
-                        double match0 = cvMatchShapes(seq0, seqT, 3);
+                        double match0 = cvMatchShapes(seq0, seqCircle, 3);
                         if(match0<matchMax){
                                 matchMax = match0;
                                 seqM = seq0;
@@ -128,11 +154,62 @@ void testMatch(IplImage* original, IplImage* templ)
 
 		printf("radius = %d centerX = %d centerY = %d", (xmax - xmin)/2, xmin+(xmax-xmin)/2, ymin+(ymax-ymin)/2);
 		
+        cvNamedWindow( "maxMatchContourCircle", 1 );
+		cvShowImage( "maxMatchContourCircle", maxMatchContourImage);
+		cvNamedWindow( "original with matches circle", 1 );
+		cvShowImage( "original with matches circle", rgb);
 
-        cvNamedWindow( "maxMatchContour", 1 );
-		cvShowImage( "maxMatchContour", maxMatchContourImage);
-		cvNamedWindow( "original with matches", 1 );
-		cvShowImage( "original with matches", rgb);
+
+
+		//2) ищем для Прямоугольника---------------------------------------------
+		seqM=0;
+        matchMax=1000;
+        // обходим контуры изображения 
+        counter=0;
+		i=0;
+		IplImage* matchesRectangle = cvCreateImage( cvGetSize(original), 8, 3);
+		cvConvertImage(src, matchesRectangle, CV_GRAY2BGR);
+		//cvSet(matchesRectangle, CV_RGB(255,255,255));
+        if(contoursI!=0){
+                // поиск лучшего совпадения контуров по их моментам 
+                for(CvSeq* seq0 = contoursI;seq0!=0;seq0 = seq0->h_next){
+                        double match0 = cvMatchShapes(seq0, seqRectangle, 3);
+                        if(match0<matchMax){
+                                matchMax = match0;
+                                seqM = seq0;
+                        }
+						cvMinEnclosingCircle(seq0,&point,&rad); // получим окружность содержащую контур
+
+						int     decimal,   sign;
+						char    *buffer;
+						int     precision = 4;
+						double  source = match0;
+
+						
+
+						buffer = _ecvt( source, precision, &decimal, &sign ); 
+						cvPutText(matchesRectangle, buffer, cvPointFrom32f(point), &font, CV_RGB(0,0,250));
+						printf( "source: %2.10f   buffer: '%s'  decimal: %d  sign: %d\n",
+								source, buffer, decimal, sign );
+						 
+				}
+        }
+        // рисуем найденный контур
+		maxMatchContourImage = cvCreateImage( cvGetSize(original), 8, 3);
+		cvSet(maxMatchContourImage, CV_RGB(255,255,255));
+		cvDrawContours(maxMatchContourImage, seqM, CV_RGB(250,0,0), CV_RGB(250,0,0), 0, 2, 8); // рисуем контур
+		x=0,y=0;
+
+		cvNamedWindow( "maxMatchContourRectangle", 1 );
+		cvShowImage( "maxMatchContourRectangle", maxMatchContourImage);
+		cvNamedWindow( "original with matches rectangle", 1 );
+		cvShowImage( "original with matches rectangle", matchesRectangle);
+
+
+
+
+
+
 
         // ждём нажатия клавиши
         cvWaitKey(0);
@@ -141,9 +218,10 @@ void testMatch(IplImage* original, IplImage* templ)
         cvReleaseMemStorage(&storage);
         cvReleaseImage(&src);
         cvReleaseImage(&rgb);
-        cvReleaseImage(&rgbT);
+        cvReleaseImage(&rgbCircle);
         cvReleaseImage(&binI);
-        cvReleaseImage(&binT);
+        cvReleaseImage(&binCircle);
+		cvReleaseImage(&binRectangle);
 
         // удаляем окна
         cvDestroyAllWindows();
@@ -161,7 +239,7 @@ int main(int argc, char* argv[])
         printf("[i] image: %s\n", filename);
         assert( original != 0 );
 
-        // имя шаблона задаётся вторым параметром
+        // шаблон Круг
         char* filename2 = argc >= 3 ? argv[2] : "D:\\work\\DiplomApplication2\\DiplomApplication2\\kontur0.png";
         // получаем картинку 
         templCircle = cvLoadImage(filename2, 0);
@@ -169,13 +247,22 @@ int main(int argc, char* argv[])
         printf("[i] template: %s\n", filename2);
         assert( templCircle != 0 );
 
+		//шаблон Прямоугольник
+		char* filename3 = argc >= 3 ? argv[2] : "D:\\work\\DiplomApplication2\\DiplomApplication2\\kontur1.png";
+        // получаем картинку 
+        templRectangle = cvLoadImage(filename3, 0);
+
+        printf("[i] template: %s\n", filename3);
+        assert( templRectangle != 0 );
+
 
         // сравнение
-        testMatch(original, templCircle);
+		testMatch(original, templCircle, templRectangle);
 
         // освобождаем ресурсы
         cvReleaseImage(&original);
         cvReleaseImage(&templCircle);
+		 cvReleaseImage(&templRectangle);
         // удаляем окна
         cvDestroyAllWindows();
         return 0;
